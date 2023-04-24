2bcb26a8d29f02e7e9a819baa7459ba2d06eb754
pedroalves4 <alvespedro04@gmail.com>
2023-04-23 22:06:59 -0300

    Removendo Long Parameter List na main

--- ImplementationSmells.cs
+++ ImplementationSmells.cs
@@ -2,7 +2,7 @@
 {
     internal class ImplementationSmells
     {
-        public void LongParameterListTest(string param1, string param2, string param3, string param4, string param5, string param6)
+        public void LongParameterListTest(string param1, string param2)
         {
             Console.WriteLine(param1 + param2);
         }
