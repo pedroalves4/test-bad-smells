81b02531e8a08d42f5fedac89b57989e8460db48
pedroalves4 <alvespedro04@gmail.com>
2023-04-23 21:39:32 -0300

    Introduzindo LongParameterList na main

---
+++ ImplementationSmells.cs
@@ -0,0 +1,10 @@
+﻿namespace TestDesignite
+{
+    internal class ImplementationSmells
+    {
+        public void LongParameterListTest(string param1, string param2, string param3, string param4, string param5, string param6)
+        {
+            Console.WriteLine(param1 + param2);
+        }
+    }
+}
